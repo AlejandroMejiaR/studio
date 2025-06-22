55514476ad19890d4a49facbb2db9fe3be1d11da
Alejandro Mejia <alejandro197mejia@gmail.com>
2025-06-22 16:22:39 +0000

    actualmente la conexion entre la imagen y la informacion tiene un degrad

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -35,7 +35,7 @@ const ProjectCard = ({ project, initialLikes }: ProjectCardProps) => {
   const technologiesToShow = 3;
 
   return (
-    <Card className="flex flex-col h-full shadow-md transition-all duration-300 hover:scale-105 hover:shadow-xl hover:shadow-accent/30 dark:hover:shadow-accent/20 overflow-hidden">
+    <Card className="flex flex-col h-full shadow-md transition-all duration-300 overflow-hidden hover:scale-105 hover:shadow-xl hover:shadow-accent/30 dark:hover:shadow-accent/20">
       <Link 
         href={`/projects/${project.slug}`} 
         aria-label={`View details for ${titleToDisplay}`}
@@ -48,28 +48,28 @@ const ProjectCard = ({ project, initialLikes }: ProjectCardProps) => {
               alt={titleToDisplay}
               fill
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              className="object-cover"
+              className="object-cover image-fade-mask"
             />
-            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-card to-transparent" />
         </div>
       </Link>
 
       <div className="flex flex-col justify-between flex-grow">
-        <CardContent className="p-4 flex-grow space-y-3">
-          <CardTitle className="font-headline text-xl mb-1 text-primary dark:text-foreground">
+        <CardContent className="p-4 pt-2 flex-grow space-y-2">
+          <CardTitle className="font-headline text-lg mb-1 text-primary dark:text-foreground">
             <Link 
               href={`/projects/${project.slug}`}
               onClick={handleProjectLinkClick}
+              className="hover:text-accent transition-colors"
             >
               {titleToDisplay}
             </Link>
           </CardTitle>
-          <CardDescription className="text-foreground/70 line-clamp-2 text-base">
+          <CardDescription className="text-foreground/70 line-clamp-2 text-sm">
             {shortDescriptionToDisplay}
           </CardDescription>
         </CardContent>
 
-        <CardFooter className="p-4 pt-2 mt-auto flex justify-between items-center border-t">
+        <CardFooter className="p-4 pt-0 mt-auto flex justify-between items-center border-t">
           <LikeButton projectId={project.id} initialLikes={initialLikes} />
           <div className="flex flex-wrap items-center justify-end gap-1.5 flex-1 ml-4">
             {project.category && (
