946bf6687ae9e942eeb596d76a56a1a764fe5a95
Alejandro Mejia <alejandro197mejia@gmail.com>
2025-06-27 01:22:07 +0000

    Quiero que el color de fondo de las tarjetas sean del mismo color del fo

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -22,7 +22,7 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
 
   return (
     <Link href={`/projects/${project.slug}`} className="block h-full no-underline text-inherit group">
-      <Card className="flex flex-col h-full shadow-md overflow-hidden transition-all duration-300 group-hover:shadow-xl group-hover:shadow-accent/30 dark:group-hover:shadow-accent/20 group-hover:scale-105">
+      <Card className="flex flex-col h-full bg-background shadow-md overflow-hidden transition-all duration-300 group-hover:shadow-xl group-hover:shadow-accent/30 dark:group-hover:shadow-accent/20 group-hover:scale-105">
         
         <div className="p-4 flex-grow flex flex-col">
             <CardTitle className="font-headline text-2xl text-accent">
