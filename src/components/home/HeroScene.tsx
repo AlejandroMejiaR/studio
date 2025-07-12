31b29d5b5beb50dbd938692c1f196d4d3348762d
Alejandro Mejia <alejandro197mejia@gmail.com>
2025-07-12 23:53:01 +0000

    Puedes anadir bloom a la escena?

--- src/components/home/HeroScene.tsx
+++ src/components/home/HeroScene.tsx
@@ -2,9 +2,9 @@
 "use client";
 
 import { Suspense } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
-import * as THREE from 'three';
+import { EffectComposer, Bloom } from '@react-three/postprocessing';
 import React from 'react';
 
 // This component loads and displays the GLB model.
@@ -28,9 +28,18 @@ export default function HeroScene() {
         <Model scale={[1, 1, 1]} position={[0, -2, 0]} />
       </Suspense>
 
-      {/* OrbitControls allows you to orbit around the model.
-          We can disable this later once the camera position is finalized. */}
+      {/* OrbitControls allows you to orbit around the model. */}
       <OrbitControls />
+
+      {/* Post-processing effects */}
+      <EffectComposer>
+        <Bloom 
+          luminanceThreshold={0.3} // Only objects brighter than this threshold will bloom
+          luminanceSmoothing={0.9} // Smoothness of the bloom effect
+          height={300} // Quality of the bloom effect
+          intensity={0.8} // Strength of the bloom effect
+        />
+      </EffectComposer>
     </Canvas>
   );
 }
