caddc95eff4fb2a295194270c1777f02643ec5f0
Alejandro Mejia <alejandro197mejia@gmail.com>
2025-07-13 00:05:59 +0000

    para posicionar la camara exactamente donde quiero haz que por consola s

--- src/components/home/HeroScene.tsx
+++ src/components/home/HeroScene.tsx
@@ -1,11 +1,13 @@
 
 "use client";
 
-import { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
+import { Suspense, useRef } from 'react';
+import { Canvas, useThree } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
 import React from 'react';
+import type { OrbitControls as OrbitControlsImpl } from 'three/examples/jsm/controls/OrbitControls';
+
 
 // This component loads and displays the GLB model.
 function Model(props: JSX.IntrinsicElements['group']) {
@@ -16,20 +18,33 @@ function Model(props: JSX.IntrinsicElements['group']) {
   return <primitive object={scene} {...props} />;
 }
 
+// A helper component to log camera position
+function CameraPositionLogger() {
+  const { camera } = useThree();
+  const controlsRef = useRef<OrbitControlsImpl>(null);
+
+  const logCameraPosition = () => {
+    const { x, y, z } = camera.position;
+    console.log(`Camera Position: { x: ${x.toFixed(2)}, y: ${y.toFixed(2)}, z: ${z.toFixed(2)} }`);
+  };
+
+  return <OrbitControls ref={controlsRef} onChange={logCameraPosition} />;
+}
+
 export default function HeroScene() {
   return (
-    <Canvas camera={{ position: [0, 2, 8], fov: 50 }}>
+    <Canvas camera={{ position: [0, 0, 5], fov: 30 }}>
       {/* Lights */}
-      <ambientLight intensity={1.5} />
-      <directionalLight position={[5, 5, 5]} intensity={2} />
-      <directionalLight position={[-5, -5, -5]} intensity={1} />
+      <ambientLight intensity={0} />
+      <directionalLight position={[5, 5, 5]} intensity={0} />
+      <directionalLight position={[-5, -5, -5]} intensity={0} />
       
       <Suspense fallback={null}>
         <Model scale={[1, 1, 1]} position={[0, -2, 0]} />
       </Suspense>
 
       {/* OrbitControls allows you to orbit around the model. */}
-      <OrbitControls />
+      <CameraPositionLogger />
 
       {/* Post-processing effects */}
       <EffectComposer>
@@ -37,7 +52,7 @@ export default function HeroScene() {
           luminanceThreshold={0.3} // Only objects brighter than this threshold will bloom
           luminanceSmoothing={0.9} // Smoothness of the bloom effect
           height={300} // Quality of the bloom effect
-          intensity={0.8} // Strength of the bloom effect
+          intensity={0.1} // Strength of the bloom effect
         />
       </EffectComposer>
     </Canvas>
