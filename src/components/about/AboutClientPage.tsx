1960be12da35407b452cc8789217bb33847c8921
Alejandro Mejia <alejandro197mejia@gmail.com>
2025-07-21 01:52:44 +0000

    cambiala nuevamente por esta https://xtuifrsvhbydeqtmibbt.supabase.co/st

--- src/components/about/AboutClientPage.tsx
+++ src/components/about/AboutClientPage.tsx
@@ -35,7 +35,7 @@ const AboutClientPage = () => {
   }
 
   const profileImage = {
-    src: "https://xtuifrsvhbydeqtmibbt.supabase.co/storage/v1/object/public/documents//Prueba.png",
+    src: "https://xtuifrsvhbydeqtmibbt.supabase.co/storage/v1/object/public/documents//AboutMe.png",
     alt: "Alejandro Mejia Rojas Portrait",
     rotation: "rotate-2",
     hint: "professional portrait"
