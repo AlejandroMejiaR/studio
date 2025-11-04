a33b0f38346c68110f200c5e8f232e3fd9af1b45
Alejandro Mejia <alejandro197mejia@gmail.com>
2025-11-04 02:51:03 +0000

    Respetar los espacios en los textos de los proyectos

--- src/components/home/ProjectDetailVew.tsx
+++ src/components/home/ProjectDetailVew.tsx
@@ -166,7 +166,7 @@ const ProjectDetailView = ({ project, onClose }: { project: Project; onClose: ()
                                             </div>
                                         </DialogTrigger>
                                     </CarouselItem>
-                                ))}
+                                )))}
                             </CarouselContent>
                              <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2 z-10 bg-black/30 hover:bg-black/50 text-white border-0" />
                             <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2 z-10 bg-black/30 hover:bg-black/50 text-white border-0" />
